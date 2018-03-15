2663b3fed997b4b154940459367acf0828fd33ca
miguel dias <fc46427@alunos.fc.ul.pt>
2018-03-15 11:09:58 +0000

    1o commit pr=ivate\]

---
+++ trabalho1/src/trabalho1/Persistent.java
@@ -0,0 +1,24 @@
+package trabalho1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Handles the interaction of an object with the persistent storage
+ * @author migdi
+ *
+ */
+public interface Persistent {
+	
+	/**
+	 * loads the object from persistent memory
+	 * @throws IOException 
+	 */
+	public void load() throws IOException;
+	
+	/**
+	 * saves the object to persistent memory
+	 */
+	public void save();
+
+}
